e9637087a8b10ec9d05ca63600cc961c5a6090d5
denilsongps13-lab <denilson.gps13@gmail.com>
2026-09-14 22:06:23 -0300

    feat: add Mega Brain autonomous orchestration core

---
+++ engine/orchestration/__init__.py
@@ -0,0 +1,4 @@
+"""Mega Brain native orchestration inspired by proven Ruflo patterns."""
+from .autopilot import Autopilot, QualityGate, SwarmRole
+
+__all__ = ["Autopilot", "QualityGate", "SwarmRole"]
